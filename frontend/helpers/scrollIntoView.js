c078aefab3e5ba2110099cd6fb7abc4751b56170
Maxi <maxim@creonit.ru>
2021-01-28 15:43:33 +0700

    Обновлён метод для скрола элемента во вью.
    Теперь поддерживает скрол внутри блока.

--- frontend/helpers/scrollIntoView.js
+++ frontend/helpers/scrollIntoView.js
@@ -1,19 +1,23 @@
 /**
- *
- * @param {HTMLElement} element - элемент который надо заскролить
- * @param {Object} [offsets] - сдвиги
- * @param {Number} [offsets.x] - сдвиг по горизонтали
- * @param {Number} [offsets.y] - сдвиг по вертикали
+ * Метод для скрола
+ * @param {HTMLElement} element элемент для показа
+ * @param {Object} [options] сдвиги
+ * @param {number} [options.y] сдвиг по вертикали
+ * @param {'smooth' | 'auto'} [options.behavior] сдвиг по вертикали
+ * @param {Window | HTMLElement} [overflowElement] контейнер для скрола
  */
-export default function scrollIntoView(element, offsets = {}) {
-    const { y = 0 } = offsets;
+export default function scrollIntoView(element, options, overflowElement = window) {
+    const { y = 0, behavior = 'smooth' } = options || {};
+    const top = overflowElement === window ? element.getBoundingClientRect().top : element.getBoundingClientRect().top - overflowElement.getBoundingClientRect().top;
+
     if (typeof element.scrollTo === 'function') {
         window.scrollTo({
-            top: element.getBoundingClientRect().top + window.pageYOffset + y,
-            behavior: 'smooth'
+            top: top + (overflowElement.pageYOffset || overflowElement.scrollTop) + y,
+            behavior
         });
+
     } else {
-        (document.scrollingElement || document.documentElement).scrollTop = element.getBoundingClientRect().top + window.pageYOffset + y;
+        (overflowElement === window ? (document.scrollingElement || document.documentElement) : overflowElement).scrollTop = top + (overflowElement.pageYOffset || overflowElement.scrollTop) + y;
     }
 
     return element;
