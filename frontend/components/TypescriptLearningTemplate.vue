5a6fd2b4c2753709e9f54bc6e6342ebdeaa4f4f2
Maxi <maxim@creonit.ru>
2021-03-30 13:34:05 +0700

    Исправлен нейминг + дополнена инструкция

--- frontend/components/TypescriptLearningTemplate.vue
+++ frontend/components/TypescriptLearningTemplate.vue
@@ -32,7 +32,7 @@ import PageService from '~/services/Page';
 import { PageStore } from '~/store';
 
 // Интерфейс описывает все данные компонента, которы обычно определяем в `data()`
-interface ComponentData {
+interface TestComponentData {
     someDataContent: string
     testingData: string|null
     page: PageData|null
@@ -64,30 +64,30 @@ interface ComponentData {
     }
 })
 // Не забываем `implements ComponentData` чтобы TS ругнулся если вы забыли определить какое-то поле в классе
-export default class TestComponent extends Vue implements ComponentData {
-    /** Человеческое описание свойства */
-    @Prop({ type: String, default: null })
+export default class TestComponent extends Vue implements TestComponentData {
+    /** Человеческое описание свойства. Документируйте за что отвечает проп. Комментарий надо писать именно в `\/** *\/` чтобы корректно ТС подсказывал */
+    // Всегда в декораторе нужно указывать тип данных, иначе Vue не будет знать о том что там внутри ожидается
+    @Prop({ type: String, required: true })
     readonly title!: string;
 
-    // Всегда в декораторе нужно указывать тип данных, иначе Vue не будет знать о том что там внутри ожидается
-    @Prop({ type: String, default: null })
+    // Так же у необязательный свойств нужно указывать дефолтное значение `null`, если вы хотите присвоить это значение сразу в свойство `data`. Смотрите дальше `someDataContent`
+    @Prop({ type: String })
     readonly customProp!: string;
 
-    // Для обязательных полей или с дефолтным значением, необъодимо ставить восклицательный знак
-    // А так же если хотим присвоить значение пропа data-свойству при инициализации
+    // Настройки пропа педавать необходимо в параметре к декоратору.
     @Prop({ type: String, default: 'testingProp: Дефолтное значение' })
     readonly testingProp!: string;
 
     // Чтобы обратиться к пропу и присвоить его значение в `data()`, это свойство нужно определить с восклицательным знаком `readonly title!: string;`
     // Для таких пропов необходимо обязательно поставить `default: null` иначе свойство не будет создано и ошибка рендера появится
-    someDataContent: ComponentData['someDataContent'] = this.title;
+    someDataContent: TestComponentData['someDataContent'] = this.title;
     // Иначе будет ошибка
-    // someDataContent: ComponentData['someDataContent'] = this.customProp;
+    // someDataContent: TestComponentData['someDataContent'] = this.customProp;
 
     // Допустим вам надо создать поле в дате, чтобы заполнить его при фетче
-    // Используйте `null` значение, чтобы переменная была инициализирована реактивно
+    // Используйте `null` значение, чтобы переменная была инициализирована
     // https://class-component.vuejs.org/guide/class-component.html#data
-    testingData: ComponentData['testingData'] = null;
+    testingData: TestComponentData['testingData'] = null;
 
     // Все методы должны указывать возвращаемый тип данных
     setTestingData(): void {
@@ -95,7 +95,7 @@ export default class TestComponent extends Vue implements ComponentData {
     }
 
 
-    page: ComponentData['page'] = null;
+    page: TestComponentData['page'] = null;
 
     printLogs(): void {
         console.log('page / from data :: ', this.page || 'Запрос ещё не выполнился', '\n\n');
