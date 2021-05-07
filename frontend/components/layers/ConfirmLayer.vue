e516285a0a4e66b7f756b69916df33519a81d902
Yuliya Logunova <yuliyalogunovaa@gmail.com>
2021-05-07 14:59:48 +0400

    Добавлены дефолтные стили для модалок

--- frontend/components/layers/ConfirmLayer.vue
+++ frontend/components/layers/ConfirmLayer.vue
@@ -1,25 +1,27 @@
 <template>
-    <div class="layer">
-        <div class="layer-container">
-            <div v-if="title" v-html="title"></div>
-            <div v-if="description" v-html="description"></div>
-            <div v-if="message" v-html="message"></div>
-            <div>
-                <button ref="acceptActionElement" type="button" @click.prevent="$emit('close', true)">
-                    <span>{{ acceptButtonCaption }}</span>
-                </button>
-                <button type="button" @click.prevent="$emit('close')">
-                    <span>{{ declineButtonCaption }}</span>
-                </button>
-            </div>
+    <BaseLayer>
+        <template #header>
+            <div v-if="title" class="mb-8" v-html="title"></div>
+        </template>
+        <div v-if="description" v-html="description"></div>
+        <div v-if="message" v-html="message"></div>
+        <div>
+            <button ref="acceptActionElement" type="button" @click.prevent="$emit('close', true)">
+                <span>{{ acceptButtonCaption }}</span>
+            </button>
+            <button type="button" @click.prevent="$emit('close')">
+                <span>{{ declineButtonCaption }}</span>
+            </button>
         </div>
-    </div>
+    </BaseLayer>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Ref, Prop } from 'nuxt-property-decorator';
+import BaseLayer from '~/components/layers/BaseLayer.vue';
 
 @Component({
+    components: { BaseLayer },
     mounted(this: ConfirmLayer) {
         this.acceptActionElement.focus();
     }
